eb072098d76b89a63eebc8ed72e746613052b3a0
dhimanpaulchowdhury <dhimanpaulshowdhury@hotmail.com>
2015-09-07 15:19:47 +0530

    Initialization

---
+++ tapdemomvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tapdemomvc.Startup))]
+namespace tapdemomvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
